2ab5e6a61fcad483ba7f5a83d8f5fbfd4b84bd4c
islam ibrahiem <islamibrahiem93@gmail.com>
2021-10-07 16:33:53 +0200

    add demo apk

---
+++ src/main/java/Demo_apk/TestMobileApp/DemoMobileApp.java
@@ -0,0 +1,30 @@
+package Demo_apk.TestMobileApp;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class DemoMobileApp {
+
+	public static void main(String[] args) throws MalformedURLException {
+		File appDir = new File("src");
+		File app = new File(appDir,"ApiDemos-debug.apk");
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		//cap.setCapability(MobileCapabilityType.DEVICE_NAME, "TestEmulator");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel");
+
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+
+	}
+
+}
